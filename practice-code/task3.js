e8d36f910ae804ac37aec928799ea14b3f573de4
Ramit Roshann <ramitroshan777777@gmail.com>
2025-09-15 20:18:52 +0530

    update server.js inside the product-management-app

--- practice-code/task3.js
+++ practice-code/task3.js
@@ -1,29 +1,47 @@
-//Ques) Set up an Express  server using Express JS
+// //Ques) Set up an Express  server using Express JS
+
+// //to run this code we have to install express first 
+// const express = require('express');
+// const app = express();
+// const port = 3060;
+
+// app.get("/", (req, res) => {
+//     res.status(200).send("Welcome to Hindu Rastra");
+// });
+
+// app.get("/contact", (req, res) => {
+//     res.status(200).send("email - hindu@rastra.com");
+// });
+
+// const users = [
+//     {id: 1, name: 'Veer Savarkar'},
+//     {id: 2, name: 'NathuRam Godse'},
+// ];
+
+// app.get("/users", (req, res) => {
+//     res.json(users);
+// });
+
+// //used to run the server
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// })
 
 
-//to run this code we have to install express first 
-const express = require('express');
-const app = express();
-const port = 3060;
+// const p1 = {id:1, name:'marker', price:20};
+// const obj = {price:25};
 
-app.get("/", (req, res) => {
-    res.status(200).send("Welcome to Hindu Rastra");
-});
+// Object.assign(p1, obj);
 
-app.get("/contact", (req, res) => {
-    res.status(200).send("email - hindu@rastra.com");
-});
+// console.log(p1);
 
-const users = [
-    {id: 1, name: 'Veer Savarkar'},
-    {id: 2, name: 'NathuRam Godse'},
-];
+// const obj2 = {name: 'white board marker', price: 30};
+// Object.assign(p1, obj2);
+// console.log(p1);
 
-app.get("/users", (req, res) => {
-    res.json(users);
-});
 
-//used to run the server
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+const id = Date.now().toString(36) + Math.random().toString(36).substr(2);
+console.log(id);
+
+const id1 = crypto.randomUUID();
+console.log(id1);
