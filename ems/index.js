ad8954d37d85135ded2a17ce6199e3a624928c70
Ramit Roshann <ramitroshan777777@gmail.com>
2025-09-30 12:20:02 +0530

    split the code in diff files using export and import(require)

---
+++ ems/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const configureDB = require('./config/db');  // import it from config/db
+const app = express();
+const port = 4040;
+
+//middle-ware
+app.use(express.json());
+
+configureDB();
+
+//start the server
+app.listen(port, () => {
+    console.log(`Server is running on port number ${port}`)
+})
