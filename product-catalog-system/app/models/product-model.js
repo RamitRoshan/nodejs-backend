b094836792e7f665c6868e950277381a10b741c2
Ramit Roshann <ramitroshan777777@gmail.com>
2025-10-26 01:26:57 +0530

    connect code with database, install third party packages

--- product-catalog-system/app/models/product-model.js
+++ product-catalog-system/app/models/product-model.js
@@ -1,14 +1,73 @@
+// const mongoose = require('mongoose');
+// const {Schema, model} = mongoose;
+
+// const userSchema = new Schema({
+//     username: String,
+//     email: String,
+//     password: String,
+
+//     role: {
+//         type: String,
+
+//     }
+// }, {timestamps: true});
+
+
+// const User = model('User', userSchema);
+
+// module.exports = User;
+
+
+ 
+
 const mongoose = require('mongoose');
-const {Schema, model} = mongoose;
+const { Schema, model } = mongoose;
 
-const userSchema = new Schema({
-    username: String,
-    email: String,
-    password: String,
-});
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Product name is required'],
+    trim: true,
+    maxlength: 200
+  },
+  price: {
+    type: Number,
+    required: [true, 'Product price is required'],
+    min: [0, 'Price must be positive']
+  },
+  description: {
+    type: String,
+    trim: true,
+    maxlength: 2000
+  },
+  image: {
+    type: String, // store filename or URL from multer upload
+    default: ''
+  },
+  isApproved: {
+    type: Boolean,
+    default: false // moderators' products default to false
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false // for soft deletion
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  approvedBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+}, { timestamps: true });
+
+// optional: add text index to support search by name and description
+productSchema.index({ name: 'text', description: 'text' });
+
+const Product = model('Product', productSchema);
+module.exports = Product;
 
 
-
-const User = model('User', userSchema);
-
-module.exports = User;
+ 
